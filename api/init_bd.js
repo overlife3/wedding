57d62c21fdb050731155b0c639fe0c4d5e2fbb73
overlife3 <vip.kostya.semenov.2004@mail.ru>
2025-02-14 08:06:07 +0300

    init bd

--- api/init_bd.js
+++ api/init_bd.js
@@ -2,15 +2,16 @@ import { sql } from "@vercel/postgres";
 
 export default async function handler(request, response) {
   if (request.method === "POST") {
-    const { question1, question2 } = request.body;
-
     try {
       // Вставляем данные в таблицу survey_results
       await sql`
-            INSERT INTO survey_results (question1, question2)
-            VALUES (${question1}, ${question2})
+            CREATE TABLE survey (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                isPresent BOOLEAN NOT NULL DEFAULT FALSE
+            );
           `;
-      response.status(200).json({ message: "Данные сохранены!" });
+      response.status(200).json({ message: "Таблица создана!" });
     } catch (error) {
       console.error("Ошибка:", error);
       response.status(500).json({ error: "Ошибка сервера" });
--- js/form.js
+++ js/form.js
@@ -3,7 +3,9 @@ const surveyForm = document.querySelector('[data-form="survey"]');
 surveyForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const response = await fetch("/api/survey");
+  const response = await fetch("/api/init_bd", {
+    method: "POST",
+  });
   const result = await response.json();
   alert(result.message);
 });
